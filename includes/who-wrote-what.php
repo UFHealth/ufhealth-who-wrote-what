bd64eeed31f5a885097c2762d1fb78d89b81f137
Chris Wiegman <cwiegman@ufl.edu>
2017-06-28 13:01:50 -0400

    Add file header and namespace

---
+++ includes/who-wrote-what.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Adds functions and hooks to filter User table
+ *
+ * @since 1.0
+ *
+ * @package UFHealth
+ *
+ * @author Chris Wiegman <cwiegman@ufl.edu>
+ */
+
+namespace UFHealth\who_wrote_what;
